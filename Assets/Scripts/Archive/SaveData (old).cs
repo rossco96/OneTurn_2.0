590057da833ea80acc37b6aac0dee1d10c770b96
rossco96 <coles.ross@gmail.com>
2024-06-26 22:15:22 +0100

    Archiving / deleting / moving existing files

---
+++ Assets/Scripts/Archive/SaveData (old).cs
@@ -0,0 +1,10 @@
+/*
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SettingsSaveData_", menuName = "Data/Save Data/Settings Save Data (key, default val)")]
+public class SaveData : ScriptableObject
+{
+	public string Key;
+	public string DefaultValue;
+}
+//*/
