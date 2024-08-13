54be42343b85d92506483071c8b30e26e8337bc7
rossco96 <coles.ross@gmail.com>
2024-08-13 12:55:57 +0100

    Tidy up Chaser

--- Assets/Scripts/Interactable/Chaser.cs
+++ Assets/Scripts/Interactable/Chaser.cs
@@ -2,12 +2,6 @@ public class Chaser : Interactable_Base
 {
 	protected override void PlayerEnter()
 	{
-		//m_canInteract = false;
-		// [Q] WHAT ARE WE DOING HERE? IF ANYTHING? CAN WE JUST LEAVE IT BLANK?
-	}
 
-	//protected override void PlayerExit()
-	//{
-	//	m_canInteract = true;
-	//}
+	}
 }
